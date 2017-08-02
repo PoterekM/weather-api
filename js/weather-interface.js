1b6c71ae3c80c84e6b9e83ba86de00b6e61bcafb
Michelle Poterek <michelle@>
2017-08-01 17:26:01 -0700

    add location to display in weather.html and add api key

---
+++ js/weather-interface.js
@@ -0,0 +1,12 @@
+var apiKey = "a928e20f76a0b5e801f9d90feb578f6a";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+    });
+  });
+});
