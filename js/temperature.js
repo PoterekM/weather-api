151976108c53512f0a7282d6ce70cf745e226cb9
Michelle Poterek <michelle@>
2017-08-01 18:00:47 -0700

    WIP: add temperature display- need to move to business logic

--- js/temperature.js
+++ js/temperature.js
@@ -3,3 +3,15 @@
 // Celsius = Kelvin - 273.15
 
 // fahrenheit = kelvin * 9/5 - 459.67
+
+
+Fahren = function(degrees) {
+  this.degrees = degrees;
+}
+
+Fahren.prototype.calculate = function() {
+  var temperature =
+}
+
+
+exports.farenModule = Faren;
--- js/weather-interface.js
+++ js/weather-interface.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
       $('.showKelvin').text("The temperature in " + city + " is " + response.main.temp + "degrees Kelvin");
+      $('.showCel').text("The temperature in " + city + " is " + ((response.main.temp) - 273) + "degrees Kelvin");
+      $('.showFahr').text("The temperature in " + city + " is " + ((response.main.temp) * (9/5) - 459.67) + "degrees fahrenheit");
     });
   });
 });
